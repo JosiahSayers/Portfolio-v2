de60d56f631f3b212a9aceef0611372abe6e3bfa
Josiah Sayers <josiah.sayers15@gmail.com>
2021-01-18 11:09:12 -0500

    adding bulma

---
+++ src/sections/intro/Intro.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<section>
+    <h1>Hello Awesome Person</h1>
+</section>
+
+<style>
+    section {
+        height: 100vh;
+        background-color: red;
+    }
+</style>
