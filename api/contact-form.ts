0421919bbdfb25bb295c39a210180afbe8baa408
Josiah Sayers <josiah.sayers15@gmail.com>
2021-01-21 16:35:15 -0500

    adding contact email serverless function

---
+++ api/contact-form.ts
@@ -0,0 +1,46 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import MailerSend, { Recipient, EmailParams } from 'mailersend';
+
+export default (req: NowRequest, res: NowResponse) => {
+    if (req.method === 'POST') {
+        if (!req.body.name) { return res.status(400).json({ msg: 'name must be provided on the body' }); }
+        if (!req.body.email) { return res.status(400).json({ msg: 'email must be provided on the body' }); }
+        if (!req.body.message) { return res.status(400).json({ msg: 'message must be provided on the body' }); }
+        if (process.env.VERCEL_ENV !== 'development' && !/(https:\/\/)?(www.)?josiahsayers\.com/.test(req.headers.origin)) { return res.status(401).send(''); }
+
+        const mailersend = new MailerSend({
+            api_key: process.env.MAILERSEND_API_KEY
+        });
+        const recipients = [new Recipient(process.env.TO_EMAIL, "Josiah Sayers")];
+        const emailParams = new EmailParams()
+            .setRecipients(recipients)
+            .setTemplateId(process.env.TEMPLATE_ID)
+            .setVariables([
+                {
+                    email: process.env.TO_EMAIL,
+                    substitutions: [
+                        {
+                            var: 'name',
+                            value: req.body.name
+                        },
+                        {
+                            var: 'email',
+                            value: req.body.email
+                        },
+                        {
+                            var: 'message',
+                            value: req.body.message
+                        }
+                    ]
+                }
+            ]);
+        
+        mailersend.send(emailParams).then((emailRes) => {
+            return res.status(200).send('');
+        }).catch((err) => {
+            return res.status(500).json(err);
+        });
+    } else {
+        return res.status(404).send('');
+    }
+}
