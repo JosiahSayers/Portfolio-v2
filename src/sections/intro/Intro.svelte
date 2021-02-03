3240fd79ea84f9b69d931898ba9e60b6d7c8edd8
Josiah Sayers <josiah.sayers15@gmail.com>
2021-02-02 21:24:39 -0500

    styling intro component

--- src/sections/intro/Intro.svelte
+++ src/sections/intro/Intro.svelte
@@ -14,10 +14,27 @@
 </script>
 
 <Hero {id} heroStyle="is-primary is-bold">
-    <h1 class="title is-unselectable">
-        Hello, <ChangingText entries={changingTextEntries} />
-    </h1>
-    <h2 class="subtitle">
-        I'm Josiah, and I'm a web developer. Let's get to know each other!
-    </h2>
+    <div class="neumorph-box">
+        <h1 class="title is-unselectable">
+            Hello, <ChangingText entries={changingTextEntries} />
+        </h1>
+        <h2 class="subtitle">
+            I'm Josiah, and I'm a web developer. Let's get to know each other!
+        </h2>
+    </div>
 </Hero>
+
+<style>
+    .neumorph-box {
+        width: fit-content;
+        padding: 1.5rem;
+    }
+
+    @media only screen and (max-width: 500px) {
+        h1 {
+            min-height: 8rem;
+            display: flex;
+            flex-direction: column;
+        }
+    }
+</style>
