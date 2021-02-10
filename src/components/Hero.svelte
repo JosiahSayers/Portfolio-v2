462ba3a7f3c143c46925fbfac8e383d3098fd9c2
Josiah Sayers <josiah.sayers15@gmail.com>
2021-02-09 19:16:52 -0500

    chaning up intro hero styling

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,12 +2,24 @@
     export let heroStyle: string = "is-success";
     export let id: string = "";
     export let fullHeight = true;
+    export let fadeOut = false;
 </script>
 
 <section class="hero {heroStyle}" class:is-fullheight={fullHeight} {id}>
-    <div class="hero-body">
+    <div class="hero-body" class:fade-out={fadeOut}>
         <div class="container">
             <slot />
         </div>
     </div>
 </section>
+
+<style>
+    .fade-out {
+        background-image: linear-gradient(
+            to bottom,
+            rgba(255, 255, 255, 0) 0%,
+            rgba(255, 255, 255, 0.2) 40%,
+            rgba(255, 255, 255, 1) 100%
+        );
+    }
+</style>
