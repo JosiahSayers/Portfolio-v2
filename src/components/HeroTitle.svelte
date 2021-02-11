02ef9bc32acdba73cc83af343e04495a283d71b0
Josiah Sayers <josiah.sayers15@gmail.com>
2021-02-10 19:36:39 -0500

    my wife said the style looked bad

--- src/components/HeroTitle.svelte
+++ src/components/HeroTitle.svelte
@@ -1,9 +1,15 @@
-<div class="has-background-primary" />
+<div class="accent" />
 <h1 class="title has-text-weight-semi-bold"><slot /></h1>
 
 <style>
-    div {
+    .title {
+        font-size: 1.9rem;
+    }
+
+    .accent {
         height: 0.3rem;
-        width: 2rem;
+        width: 2.3rem;
+        background-color: #a8a8a8;
+        margin-bottom: 0.45rem;
     }
 </style>
